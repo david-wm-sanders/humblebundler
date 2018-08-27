77ef18e8439fa52701e644ba87009b71d9198cd3
David Sanders <david.wm.sanders@gmail.com>
2018-08-27 04:25:28 +0100

    Add existing source

---
+++ get_bundle.py
@@ -0,0 +1,72 @@
+"""Gets a Humble Bundle
+
+Usage:
+    get_bundle.py <htmlpath> <outputdir>
+"""
+
+import subprocess
+from pathlib import Path
+from urllib.parse import urlparse
+
+import requests
+from bs4 import BeautifulSoup
+from docopt import docopt
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    # Load and parse the html file
+    html_p = Path(args["<htmlpath>"])
+    with html_p.open(mode="r", encoding="utf-8") as html_f:
+        html_doc = html_f.read()
+    soup = BeautifulSoup(html_doc, "html.parser")
+
+    # Find all donwload button divs
+    download_btn_divs = soup.find_all("div", class_="flexbtn active noicon js-start-download")
+
+    # Filter out download for PDF and video (Download) files
+    valid_span_values = ["PDF", "Download"]
+    links = []
+    for dl_btn_div in download_btn_divs:
+        if dl_btn_div.span.text in valid_span_values:
+            links.append(dl_btn_div.a["href"])
+
+    # Setup output directory
+    dir_p = Path(args["<outputdir>"])
+    dir_torrents_p = dir_p / "torrents"
+    if not dir_torrents_p.exists():
+        dir_torrents_p.mkdir(parents=True, exist_ok=True)
+
+    # Download the .torrent files with requests
+    torrent_paths = []
+    for link in links:
+        # Extract the filename from the link URL
+        filename = urlparse(link).path.split("/")[-1]
+        torrent_p = dir_torrents_p / filename
+        if torrent_p.exists():
+            print(f"Skipping '{filename}' as it exists already")
+        else:
+            # Make a get request for the torrent file
+            print(f"Downloading '{filename}'...")
+            r = requests.get(link, stream=True)
+            with torrent_p.open(mode='wb') as f:
+                for chunk in r.iter_content(chunk_size=128):
+                    f.write(chunk)
+        torrent_paths.append((filename, str(torrent_p)))
+
+    # Fire up deluged
+    print("Firing up deluged...")
+    subprocess.run(["deluged"])
+
+    # Add torrents to deluge
+    deluge_add_command = ["deluge-console", "add", "-p", str(dir_p)]
+    deluge_add_command.extend([torrent_path[1] for torrent_path in torrent_paths])
+    print("Adding torrents to deluge...")
+    subprocess.run(deluge_add_command, stdout=subprocess.PIPE)
+
+    # Start (/resume) all the torrents that were just added
+    print("Starting torrents...")
+    deluge_resume_command = ["deluge-console", "resume"]
+    deluge_resume_command.extend([torrent_path[0].rstrip(".torrent") for torrent_path in torrent_paths])
+    subprocess.run(deluge_resume_command)
